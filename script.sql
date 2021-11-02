51b1fc6b96ceab5a15c44e75ab3f783300c94373
diego.narvaez.barete.contractor <diego.narvaez.barete.contractor@bbva.com>
2021-11-02 14:13:29 -0600

    Se agrega script SQL

---
+++ script.sql
@@ -0,0 +1,7 @@
+show databases;
+use db_springboot_backend;
+select *  from clientes;
+
+desc clientes;
+
+update clientes set apellido='Guzmán', create_at='2018-01-01', email='john.doe@gmail.com', nombre='Andrés2' where id=1;
