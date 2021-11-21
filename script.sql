b5c053de042f79af0ec0771c97b3d9c0f12e3221
diego.narvaez.barete.contractor <diego.narvaez.barete.contractor@bbva.com>
2021-11-21 14:52:22 -0600

    Se agrega querys

--- script.sql
+++ script.sql
@@ -1,7 +1,8 @@
 show databases;
 use db_springboot_backend;
 select *  from clientes;
-
+select *  from regiones;
+select nombre, apellido from clientes where region_id =3;
 desc clientes;
 
 #update clientes set apellido='Guzmán', create_at='2018-01-01', email='john.doe@gmail.com', nombre='Andrés2' where id=1;
